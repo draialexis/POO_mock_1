f37d43d8cf72708d59038eb6152c5f230b8915fa
Alexis DRAI <drai.alexis@gmail.com>
2022-06-05 05:54:13 +0200

    Fix #1

--- src/com/alexisdrai/ex1/Plop.java
+++ src/com/alexisdrai/ex1/Plop.java
@@ -3,9 +3,14 @@ package com.alexisdrai.ex1;
 public class Plop {
     private final String a = "a";
     private final int    r;
-    //    private final int    s;
+    private final int    s;
 
-    public Plop(int v) {
+    //    public Plop(int v) {
+    //        this.r = v;
+    //    }
+
+    public Plop(int v, int w) {
         this.r = v;
+        this.s = w;
     }
 }
